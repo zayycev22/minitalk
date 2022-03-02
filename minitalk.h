fa261c56e700dd598e8ad03731814a0e42223a04
zayycev22 <zayycev22@gmail.com>
2022-03-02 06:11:35 +0300

    minitalk header

---
+++ minitalk.h
@@ -0,0 +1,13 @@
+//
+// Created by zayycev22 on 02.03.2022.
+//
+
+#ifndef MINITALK_MINITALK_H
+#define MINITALK_MINITALK_H
+#include "ft_printf/includes/ft_printf.h"
+#include <unistd.h>
+# include <signal.h>
+# include <string.h>
+#include <stdlib.h>
+
+#endif //MINITALK_MINITALK_H
